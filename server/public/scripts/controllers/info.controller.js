5419ef3819c019b296e1c9ec22af2b0b015b0406
Daniel Shugarts <dshugarts@fluidhealthandfitness.com>
2019-09-02 15:13:52 -0500

    delete

--- server/public/scripts/controllers/info.controller.js
+++
@@ -1,6 +0,0 @@
-myApp.controller('InfoController', ['UserService', function (UserService) {
- // console.log('InfoController created');
-  var self = this;
-  self.userService = UserService;
-
-}]);
